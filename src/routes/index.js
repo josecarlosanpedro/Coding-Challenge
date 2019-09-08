1d72f826889cab199c44d2243b6c9754854c635a
unknown <josecarlosanpedro>
2019-09-08 09:46:11 +0800

    add manifest for icon

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,8 @@
-import React, { useState, useEffect, Fragment, Suspense, lazy } from 'react';
+import React, { Fragment, Suspense } from 'react';
 import PropTypes from 'prop-types';
 import PublicRoutes from './public';
 import PrivateRoutes from './private';
 import Loading from './Loading'
-
 
 const propTypes = {
   isLoggedIn: PropTypes.bool.isRequired,
