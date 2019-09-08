8509f2185e3d7193c70d1f92c6b7919c96636bb0
unknown <josecarlosanpedro>
2019-09-08 09:14:06 +0800

    enable service worker and fix logged in

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,26 +1,20 @@
 import React, { useState, useEffect, Fragment, Suspense, lazy } from 'react';
 import PropTypes from 'prop-types';
 import PublicRoutes from './public';
+import PrivateRoutes from './private';
 import Loading from './Loading'
+
 
 const propTypes = {
   isLoggedIn: PropTypes.bool.isRequired,
 };
 
 const Routes = ({ isLoggedIn }) => {
-  console.log(isLoggedIn, 'isLoggedIn')
-  const [PrivateRoutes, setPrivateRoutes] = useState(null);
 
-  useEffect(() => {
-    if (isLoggedIn) {
-      const privateLazy = lazy(() => import('./private'));
-      setPrivateRoutes(privateLazy);
-    }
-  }, [isLoggedIn]);
   return (
     <Fragment>
       <PublicRoutes isLoggedIn={isLoggedIn} />
-      {isLoggedIn && PrivateRoutes
+      {isLoggedIn
         && (
           <Suspense fallback={<Loading />}>
             <PrivateRoutes />
