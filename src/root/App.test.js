d78ddb588a257cd0c61d2f028fa773d7f39bf5d4
unknown <josecarlosanpedro>
2019-09-08 20:17:11 +0800

    setup

--- src/root/App.test.js
+++ src/root/App.test.js
@@ -1,9 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+describe('App test', () => {
+  it('Unit Test sample', () => {
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  });
 });
